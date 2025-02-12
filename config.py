c40e28a3243ace16b18c92335102e41671cf9b17
owitness <lilmacalbee@gmail.com>
2025-02-11 19:44:52 -0500

    Added RDS SQL Endpoint

---
+++ config.py
@@ -0,0 +1,7 @@
+DATABASE_CONFIG = {
+    'host': 'http://gym-database.clqqsuqke2sz.us-east-2.rds.amazonaws.com/',
+    'user': 'root',
+    'password': 'COSCAdmin',
+    'database': 'gym-database',
+    'port': 3306
+}
